aa4fb1f0998c956bae07b8367087fbc3193de2c2
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-02 11:38:34 -0400

    Adds index action and text fields for create action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      movies: [],
+      newMovieParams: {},
+    };
+  },
+  created: function () {
+    axios.get("/movies.json").then((response) => {
+      console.log(response.data);
+      this.movies = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Add a movie</h1>
+    <h3>Title:</h3>
+    <input type="text" v-model="newMovieParams.title" />
+    <h3>Year:</h3>
+    <input type="text" v-model="newMovieParams.year" />
+    <h3>Plot:</h3>
+    <input type="text" v-model="newMovieParams.plot" />
+    <h3>Director:</h3>
+    <input type="text" v-model="newMovieParams.director" />
+    <h1>Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <p>Director: {{ movie.director }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style></style>
